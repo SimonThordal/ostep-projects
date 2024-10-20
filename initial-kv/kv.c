292134d74c15da41bc87fa5c9b53eee563c71e41
SimonThordal <simonthordal@gmail.com>
2024-10-20 22:38:36 +0200

    Add a linked list implementation for the db

--- initial-kv/kv.c
+++ initial-kv/kv.c
@@ -5,26 +5,24 @@
 #define _GNU_SOURCE
 
 const char* DATABASE_FILE = "database.txt";
+const char* SEPARATOR=",";
+typedef struct node
+{
+    int key;
+    char* val;
+    struct node* next;
+} node_t;
 
 /**
- * Enter a value into the DB with a given key.
- * Expects arguments of the form p,KEY,VALUE
+ * Turn a k,v string into a node struct.
  */
-int put(char* _arg) {
-    // Skip the first tokenization as it is "p"
-    char* arg = malloc(strlen(_arg)*sizeof(char));
-    arg = strncpy(arg, _arg, strlen(_arg));
-    // Skip until the first comma as it is just the operation
-    char *c = strtok(arg, ",");
-    if (c == NULL || *c=='\0') {
-        return -1;
-    }
-    c = strtok(NULL, ",");
+int parse(char* row, node_t* node) {
+    errno = 0;
+    char *c = strtok(row, SEPARATOR);
     if (c == NULL || *c=='\0') {
         return -1;
     }
     // Convert the key to a string
-    errno = 0;
     char *endptr;
     long int key = strtol(c, &endptr, 10);
     if (errno != 0) {
@@ -37,38 +35,89 @@ int put(char* _arg) {
         return -1;
     }
     // Grab the value from the arg
-    char *val = strtok(NULL, ",");
+    char *val = strtok(NULL, SEPARATOR);
     if (val == NULL) {
         return -1;
     }
-    printf("key: %ld, val: %s\n", key, val);
+    node->key = (int)key;
+    node->val = (char*)malloc(sizeof(val));
+    strcpy(node->val, val);
+    node->next = NULL;
+    return 0;
+}
+
+/**
+ * Read in the database and return it represented as a linked list.
+ */
+int initialize(node_t* head, FILE *fp) {
+    rewind(fp);
+    errno = 0;
+    size_t n;
+    char* line = NULL;
+    size_t len = 0;
+    node_t* prev = NULL;
+    while ((n = getline(&line, &len, fp)) != -1)
+    {
+        // Read the line into the current node
+        parse(line, head);
+        // Add the current node to linked list if it is not the first node
+        if (prev != NULL) {
+            prev->next = head;
+        }
+        prev = head;
+        // Create a new node for the next line
+        node_t* node = (node_t*)malloc(sizeof(node_t));
+        if (errno != 0) {
+            fprintf(stderr, "kv: error reading database file");
+            return -1;
+        }
+        head = node;
+    }
+    return 0;
+} 
+
+
+/**
+ * Enter a value into the DB with a given key.
+ * Expects arguments of the form p,KEY,VALUE
+ */
+int put(char* _arg, node_t *head) {
+    // Skip the first tokenization as it is "p"
+    char* arg = malloc(strlen(_arg)*sizeof(char));
+    arg = strncpy(arg, _arg, strlen(_arg));
+    // Skip until the first comma as it is just the operation
+    char *c = strtok(arg, ",");
+    if (c == NULL || *c=='\0') {
+        return -1;
+    }
+    node_t *node = (node_t*)malloc(sizeof(node_t));
+    int res = parse(&arg[2], node);
+    if (res < 0) {
+        return -1;
+    }
+    while (head->next != NULL)
+    {
+        head = head->next;
+    }
+    head->next = node;
     return 0;
 }
 
 /**
  * List all k,v pairs in the database.
  */
-int all(FILE *fp) {
-    rewind(fp);
-    errno = 0;
-    size_t n;
-    char* line = NULL;
-    size_t len = 0;
-    while ((n = getline(&line, &len, fp)) != -1)
-    {
-        if (errno != 0) {
-            return -1;
-        }
-        printf("%s", line);
+void all(node_t* head) {
+    while (head != NULL) {
+        printf("%d:%s\n", head->key, head->val);
+        head = head->next;
     }
-    return 0;
 }
 
 /**
  * Get the value stored in a given key.
  * Expects arguments of the form g,KEY
  */
-char *get(char* arg) {
+char *get(char* arg, node_t* head) {
     return 0;
 }
 
@@ -87,23 +136,28 @@ int main(int argc, char* argv[]) {
             exit(EXIT_FAILURE);
         }
     }
+    node_t *head = (node_t*)malloc(sizeof(node_t));
+    if (head == NULL) {
+        fprintf(stderr, "kv: unable to allocate head");
+        exit(EXIT_FAILURE);
+    }
+    int res = initialize(head, fp);
+    if (res < 0) {
+        exit(EXIT_FAILURE);
+    }
     // Parse arguments
     int i; 
     char* arg;
     for (i=1; i<argc; i++) {
         arg = argv[i];
         if ('p' == arg[0]) {
-            int res = put(arg);
+            int res = put(arg, head);
             if (res < 0) {
                 printf("kv: failed put operation: %s\n", arg);
                 continue;
             }
         } else if ('a' == arg[0]) {
-            int res = all(fp);
-            if (res < 0) {
-                printf("kv: failed to list database entries: %s\n", arg);
-                continue;
-            }
+            all(head);
         } else {
             printf("kv: unknown operation\n");
             fclose(fp);
